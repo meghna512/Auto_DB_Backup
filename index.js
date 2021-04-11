b0a07347ea240435e4ff81117e55c3ab3f7ec3b2
meghna512 <knjmeghna512@gmail.com>
2021-04-11 16:14:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const cron = require('node-cron');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const { exec, execSync } = require('child_process');
+const path = require('path');
+
+const s3 = new AWS.S3();
+
+
+const setBackupTime = function (config) {
+    console.log("Starting to create backup");
+    if (configValidation(config)) {
+        cron.schedule(config.backupTime, () => {
+            try {
+                createBackup(config);
+            } catch (err) {
+                console.log(err);
+            }
+
+        });
+    }
+}
+
+function configValidation(config) {
+    if (config.backupTime && config.host && config.port && config.dbName && config.bucket && config.key) {
+        return true;
+    } else {
+        throw new Error("Please enter a valid input in the order of backup time, hostname, port, db name");
+    }
+}
+
+function createBackup(config) {
+    console.log("Creating Backup");
+    exec(`mongodump --host ${config.host} --port ${config.port} --db ${config.dbName}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        } else {
+            execSync(`zip -r backup *`, {
+                cwd: path.join(__dirname, "./")
+            });
+            uploadBackup(config);
+        }
+    });
+}
+
+function uploadBackup(config) {
+    console.log("Uploading Bakcup");
+    const params = {
+        Bucket: config.bucket,
+        Key: config.key
+    }
+    params.Body = fs.readFileSync(path.join(__dirname, "./backup.zip"), 'binary');
+    s3.putObject(params, function(err, data){
+        if(err){
+            throw err;
+        }else {
+            console.log(`Backup uploaded successfully at ${new Date().toISOString()}`)
+        }
+    })
+    cleanUp();
+}
+
+function cleanUp(){
+    fs.unlinkSync(path.join(__dirname, './backup.zip'));
+    fs.rmdirSync(path.join(__dirname, './dump'),{ recursive: true });
+    console.log("Clean Up Done");
+}
+
+module.exports = {
+    setBackupTime
+}
